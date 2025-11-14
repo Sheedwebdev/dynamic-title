ae5d3be50d97bb905c71d1ffe7bfbdafb7aab460
Rashid Enahora <rashidenahora@MacBookPro.attlocal.net>
2025-11-14 00:32:23 -0500

    Added another state variable to useEffect dependency array

--- src/components/DynamicTitle/DynamicTitle.jsx
+++ src/components/DynamicTitle/DynamicTitle.jsx
@@ -1,21 +1,60 @@
-import React, { useEffect, useState}  from 'react';
+import React, { useEffect, useState, useId}  from 'react';
 import styled from 'styled-components';
 
   function DynamicTitle() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+    const id = useId();
 
     useEffect(() => {
-      document.title = `${count} times`;
-    }, [count])
+      document.title = `${name} clicked ${count} times`;
+    }, [count, name])
 
 
     return (
         <Wrapper>
-          <h1>You clicked {count} times</h1>
+          <Form onSubmit={(e) => {
+            e.preventDefault()
+          }}>
+            <Input 
+              type='text'
+              placeholder='Jason'
+              id={`name-${id}`}
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <Label htmlFor={`name-${id}`}>Name</Label>
+          </Form>
           <Button onClick={() => setCount(count + 1)}>Click Me</Button>
         </Wrapper>
     );
   }
+
+  const Input = styled.input`
+    width: 250px;
+    height: 35px;
+    padding: 0;
+    line-height: 1.2;
+    font-size: 1.25rem;
+
+    &::placeholder {
+      font-size: 1.25rem;
+    }
+  `;
+
+  const Label = styled.label`
+    font-size: 1.5rem;
+    font-weight: 600;
+  `;
+
+  const Form = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  `;
    
   const Button = styled.button`
     background-color: hsl(225deg 100% 50%);
@@ -31,6 +70,7 @@ import styled from 'styled-components';
     align-items: center;
     width: 100vw;
     min-height: 100vh;
+    gap: 20px;
   `;
 
 export default DynamicTitle;
